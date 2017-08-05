f246b2517f1a5766846da09683684d49c6a2f49e
Wessel Strengholt <wessel.strengholt@gmail.com>
2017-08-05 10:40:26 +0200

    Remove test UnauthorizedPaymentException

--- tests/Nimbles/OnlineBetaalPlatform/Adapter/OnlineBetaalPlatformTest.php
+++ tests/Nimbles/OnlineBetaalPlatform/Adapter/OnlineBetaalPlatformTest.php
@@ -9,6 +9,7 @@
 namespace Tests\Nimbles\OnlineBetaalPlatform\Adapter;
 
 use GuzzleHttp\ClientInterface;
+use Nimbles\OnlineBetaalPlatform\Exception\UnauthorizedPaymentException;
 use Nimbles\OnlineBetaalPlatform\Model\Payment;
 use Nimbles\OnlineBetaalPlatform\Adapter\OnlineBetaalPlatform;
 use PHPUnit\Framework\TestCase;
@@ -128,70 +129,7 @@ class OnlineBetaalPlatformTest extends TestCase
     }
 
     /**
-     * @expectedException \Nimbles\OnlineBetaalPlatform\Exception\UnauthorizedPaymentException
-     */
-    public function testUnauthorizedPaymentException()
-    {
-        $this->payment->expects($this->once())
-            ->method('getBuyerFirstName')
-            ->willReturn('Klaas');
-
-        $this->payment->expects($this->once())
-            ->method('getBuyerLastName')
-            ->willReturn('Bruinsma');
-
-        $this->payment->expects($this->once())
-            ->method('getBuyerEmail')
-            ->willReturn('klaas@bruinsma.nl');
-
-        $this->payment->expects($this->exactly(2))
-            ->method('getAmount')
-            ->willReturn(2500);
-
-        $this->payment->expects($this->once())
-            ->method('getShippingCosts')
-            ->willReturn(100);
-
-        $this->payment->expects($this->once())
-            ->method('getReturnUrl')
-            ->willReturn('https://www.foo.com/test');
-
-        $this->payment->expects($this->once())
-            ->method('getToken')
-            ->willReturn('super-secret');
-
-        $this->httpClient->expects($this->once())
-            ->method('request')
-            ->with('POST', 'https://api.onlinebetaalplatform.nl/transactions', [
-                'auth' => ['secret-token', null],
-                'form_params' => [
-                    'buyer_name_first'      => 'Klaas',
-                    'buyer_name_last'       => 'Bruinsma',
-                    'buyer_emailaddress'    => 'klaas@bruinsma.nl',
-                    'merchant_uid'          => 'secret-id',
-                    'products' => [
-                        0 => [
-                            'name' => 'Online payment',
-                            'price' => 2500,
-                            'quantity' => 1,
-                        ],
-                    ],
-                    'shipping_costs' => 100,
-                    'total_price'    => 2500,
-                    'return_url'     => 'https://www.foo.com/test?token=super-secret',
-                ],
-            ])
-            ->willReturn($this->response);
-
-        $this->response->expects($this->once())
-            ->method('getStatusCode')
-            ->willReturn(401);
-
-        $this->onlineBetaalPlatform->createTransaction($this->payment);
-    }
-
-    /**
-     * @expectedException \Nimbles\OnlineBetaalPlatform\Exception\CreatePaymentException
+     * @expectedException Nimbles\OnlineBetaalPlatform\Exception\CreatePaymentException
      */
     public function testCreatePaymentException()
     {
